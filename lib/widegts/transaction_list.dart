f1950a5ef822aac7d47fcb80de1d72b907d0a128
AfafYa20 <Afaf.ya27@gmail.com>
2021-06-12 18:58:18 +0300

    New Project Literally :)

--- lib/widegts/transaction_list.dart
+++ lib/widegts/transaction_list.dart
@@ -5,8 +5,9 @@ import '../models/transactions.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> userTransactions;
+  final Function deletTransaction;
 
-  TransactionList(this.userTransactions);
+  TransactionList(this.userTransactions, this.deletTransaction);
 
   @override
   Widget build(BuildContext context) {
@@ -32,45 +33,36 @@ class TransactionList extends StatelessWidget {
               itemCount: userTransactions.length,
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$ ${userTransactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Container(
+                          height: 20,
+                          child: FittedBox(
+                            child: Text(
+                              '\$ ${userTransactions[index].amount.toStringAsFixed(2)}',
+                            ),
                           ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            userTransactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat()
-                                .add_yMMMMd()
-                                .format(userTransactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      userTransactions[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(userTransactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () =>
+                          deletTransaction(userTransactions[index].id),
+                      color: Theme.of(context).errorColor,
+                    ),
                   ),
                 );
               },
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,36 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
-  final Function addNewTransaction;
-  NewTransaction(this.addNewTransaction);
+  final Function addTx;
+
+  NewTransaction(this.addTx);
 
   @override
   _NewTransactionState createState() => _NewTransactionState();
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-
+  final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  DateTime _datepickered;
-
-  void _submittedTx() {
+  void _submitData() {
     if (_amountController.text.isEmpty) {
       return;
     }
-    final titleOfTx = titleController.text;
-    final amountOfTx = double.parse(_amountController.text);
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
-    if (titleOfTx.isEmpty || amountOfTx <= 0 || _datepickered == null) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.addNewTransaction(
-      titleOfTx,
-      amountOfTx,
-      _datepickered,
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
     );
+
     Navigator.of(context).pop();
   }
 
@@ -41,12 +41,12 @@ class _NewTransactionState extends State<NewTransaction> {
       initialDate: DateTime.now(),
       firstDate: DateTime(2019),
       lastDate: DateTime.now(),
-    ).then((pickeredDate) {
-      if (pickeredDate == null) {
+    ).then((pickedDate) {
+      if (pickedDate == null) {
         return;
       }
       setState(() {
-        _datepickered == pickeredDate;
+        _selectedDate = pickedDate;
       });
     });
     print('...');
@@ -55,52 +55,56 @@ class _NewTransactionState extends State<NewTransaction> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(3),
+        padding: EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
+          children: <Widget>[
             TextField(
-              decoration: InputDecoration(labelText: 'Tilte'),
-              controller: titleController,
-              keyboardType: TextInputType.text,
-              onSubmitted: (_) => _submittedTx(),
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: _amountController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submittedTx(),
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (val) => amountInput = val,
             ),
             Container(
               height: 70,
               child: Row(
-                children: [
+                children: <Widget>[
                   Expanded(
                     child: Text(
-                      _datepickered == null
-                          ? 'No selected date'
-                          : 'Picked date: ${DateFormat.yMd().format(_datepickered)}',
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                     ),
                   ),
                   FlatButton(
+                    textColor: Theme.of(context).primaryColor,
                     child: Text(
-                      'Choose date',
+                      'Choose Date',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     onPressed: _presentDatePicker,
-                    textColor: Theme.of(context).primaryColor,
                   ),
                 ],
               ),
             ),
             RaisedButton(
-              onPressed: _submittedTx,
-              child: Text('Save'),
+              child: Text('Add Transaction'),
               color: Theme.of(context).primaryColor,
               textColor: Theme.of(context).textTheme.button.color,
+              onPressed: _submitData,
             ),
           ],
         ),
